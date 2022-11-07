99968d02b5a6cb9d05f5689ccbd6640829efbf70
Dinesh <52914828+Dinesh039@users.noreply.github.com>
2022-11-07 18:36:29 +0530

    db connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
+import Homescreen from "./Screen/Homescreen";
 
 function App(props) {
   return (
-    <div>
-      <h1>Shop Me</h1>
+    <div className="App">
+      <Navbar/>
+      <Homescreen/>
+      
     </div>
   );
 }
-
+ 
 export default App;
